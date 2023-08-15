d6601e3056bb7fc68b1096aa79cdf88a1b9879da
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-15 11:31:54 +0300

    Fixing "Main" class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,10 @@
 package app;
 
-import app.entyties.Substance;
-import app.sensors.ColorSensor;
-import app.sensors.WeightSensor;
+import app.pattern.ObserverPattern;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Substance substance = new Substance();
-
-        new ColorSensor(substance);
-        new WeightSensor(substance);
-
-        System.out.println("Substance temperature +25 ");
-        substance.setState(25);
-
-        System.out.println("Substance temperature -5 ");
-        substance.setState(-5);
+        ObserverPattern.observer();
     }
 }
